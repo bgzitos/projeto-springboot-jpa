e28b055684d704c96320a99b9745f2ef136064c9
bgzitos <lucaslopes49441008@gmail.com>
2026-03-29 19:22:01 -0300

    Project created

---
+++ src/main/java/com/projetospring/bgvendas/BgvendasApplication.java
@@ -0,0 +1,13 @@
+package com.projetospring.bgvendas;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BgvendasApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BgvendasApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/projetospring/bgvendas/BgvendasApplicationTests.java
@@ -0,0 +1,13 @@
+package com.projetospring.bgvendas;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BgvendasApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
