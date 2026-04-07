96fd897b786323f15b943f208595e5abc5561834
bgzitos <lucaslopes49441008@gmail.com>
2026-04-07 19:25:23 -0300

    Exception handling - findById

---
+++ src/main/java/com/projetospring/bgvendas/services/exceptions/ResourceNotFoundException.java
@@ -0,0 +1,13 @@
+package com.projetospring.bgvendas.services.exceptions;
+
+import java.io.Serial;
+
+public class ResourceNotFoundException extends RuntimeException{
+
+    @Serial
+    private static final long serialVersionUID = 8276694179375375003L;
+
+    public ResourceNotFoundException(Object id) {
+        super("Resource not found. Id " + id);
+    }
+}
